172710ab8a11c0b246a4b935dbe5cc030fc4df53
Yassine Lafryhi <y.lafryhi@gmail.com>
2020-06-09 10:08:51 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -602,3 +602,10 @@ INSERT INTO Cases (Number, Type, Date)
 VALUES (7408, 'recovered', '08-06-2020');
 INSERT INTO Cases (Number, Type, Date)
 VALUES (8302, 'confirmed', '08-06-2020');
+
+INSERT INTO Cases (Number, Type, Date)
+VALUES (208, 'deaths', '09-06-2020');
+INSERT INTO Cases (Number, Type, Date)
+VALUES (7423, 'recovered', '09-06-2020');
+INSERT INTO Cases (Number, Type, Date)
+VALUES (8408, 'confirmed', '09-06-2020');
